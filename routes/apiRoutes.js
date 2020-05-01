be82be83b8f9232d8f216ad02cf39d7250412c7f
Eddie Saunders <saunders.eddie@outlook.com>
2020-05-01 11:45:13 -0400

    notes writing to db.json complete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,7 +14,12 @@ const fs = require('fs');
 module.exports = (app) => {
 
     app.get('/api/notes', (req, res) => {
-        res.json(notes);
+
+        console.log("Notes Contains:", notes);
+        fs.readFile('db/db.json', (err, data) => {
+            if (err) throw err;
+            res.json(JSON.parse(data));
+        });
     });
 
     app.post('/api/notes', (req, res) => {
@@ -27,27 +32,45 @@ module.exports = (app) => {
         // EXS 1st Mqy 2020 - This is naughty code, as the +1 gives us potential for duplicate IDs
         // What we should do is go through the list and reassign the ID's
         // Of course once the delete function is complete this maybe a moot point as the length and id numbers should tie up.
-        myNote.id = notes.length + 1;
+        newReference = notes.length+1
+        myNote.id = String(notes.length+1);
 
+        console.log ("Our ID String:", String(myNote.id));
         // console.log("Total Notes: ", totalNotes);
         notes.push(myNote);
+    
+
+        console.log("Notes Starts:");
+        console.log(notes);
+        console.log("Notes Ends");
+
+        myTestNotes = JSON.stringify(notes);
+        console.log("String Notes Starts:");
+        console.log(myTestNotes);
+        console.log("String Notes Ends:");
+
+        //fs.writeFile('db/db.json', notes);
+
+        fs.writeFile ('db/db.json', JSON.stringify(notes), function (err) {
+            if (err) throw err;
+            console.log ("File written");
+        });
+
         res.json(true);
-        console.log("Notes Length:", notes.length);
+
     });
 
     app.delete('/api/notes/:id', (req, res) => {
         // EXS 1st May 2020 - Get our id of the note to delete, if we only have 1 note left, then pop it.
         var idToRemove = req.params.id;
-
         if (notes.length <= 1) {
             notes.pop();
 
         } else {
             notes.splice(idToRemove - 1, 1);
-            // After we splice then rewrite the notes.id field
+            // After we splice then rewrite the notes.id field, look at ES6 arrow function for this
             notes.forEach(updateID);
         }
-
         function updateID(item, index) {
             console.log(item.id);
             item.id = index + 1;
