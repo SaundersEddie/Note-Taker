cb355030f99c128cace87db5996035266645f707
Eddie Saunders <saunders.eddie@outlook.com>
2020-05-01 08:54:05 -0400

    Renumbered id after deletion of note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,17 +2,18 @@
 // This code will route our api calls to the appropriate functions
 //
 // Eddie Saunders saunders.eddie@outlook.com 30th April 2020
-//
+
+// EXS 1st May 2020 We have to use FS in this section, getting the general flow complete first.
 
 const notes = require('../db/db.json');
-// const path = require('path');
-console.log("Our current path in apiRoutes.js:", __dirname);
-console.log(notes.length);
+const fs = require('fs');
+
+//console.log("Our current path in apiRoutes.js:", __dirname);
+//console.log(notes.length);
 
 module.exports = (app) => {
+
     app.get('/api/notes', (req, res) => {
-        //console.log(__dirname);
-        //console.log(__filename);
         res.json(notes);
     });
 
@@ -21,15 +22,14 @@ module.exports = (app) => {
         // EXS 1st May 2020 - Add our id in with the note body
         // We're going to need to do some checking here to ensure that we don't duplicate ID's
         // That code is currently not implemented.
-
-        // console.log("Req Body:", req.body);
-
         const myNote = req.body
         totalNotes = notes.length;
         // EXS 1st Mqy 2020 - This is naughty code, as the +1 gives us potential for duplicate IDs
+        // What we should do is go through the list and reassign the ID's
+        // Of course once the delete function is complete this maybe a moot point as the length and id numbers should tie up.
         myNote.id = notes.length + 1;
-        
-        console.log("Total Notes: ", totalNotes);
+
+        // console.log("Total Notes: ", totalNotes);
         notes.push(myNote);
         res.json(true);
         console.log("Notes Length:", notes.length);
@@ -40,12 +40,23 @@ module.exports = (app) => {
         var idToRemove = req.params.id;
 
         if (notes.length <= 1) {
-            console.log("Only 1 note let to delete")
             notes.pop();
 
         } else {
             notes.splice(idToRemove - 1, 1);
+            // After we splice then rewrite the notes.id field
+            notes.forEach(updateID);
         }
+
+        function updateID(item, index) {
+            console.log(item.id);
+            item.id = index + 1;
+        }
+
         res.json(true);
     })
+
+
+
 }
+
--- server.js
+++ server.js
@@ -2,12 +2,10 @@
 // assigned on our hosting
 // 
 // Eddie Saunders saunders.eddie@outlook.com 30th April 2020
-// 
 
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 9000;
-console.log ("Our current path with server.js:", __dirname);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,5 +16,4 @@ require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
   console.log('App listening on PORT: ' + PORT);
-  // console.log (app._router.stack);
 });
